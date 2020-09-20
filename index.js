0509439075ddb307b463456864f81cb754e67eda
Oscar Gross Jr <oscar.gross222@gmail.com>
2020-09-20 18:18:30 -0300

    Commit puppeteer acessando Elevor

--- index.js
+++ index.js
@@ -40,13 +40,17 @@ app.use((req,res,next) => {
 
 })
 
+app.use('/admin', admin)
+
 app.get("/" , (req, res) => {
   
     res.send("oii");
     main();
 
-
 })
+
+const dataInicial = '01/01/2020'
+const dataFinal = '01/12/2020'
 
 
 async function main() {
@@ -54,15 +58,53 @@ async function main() {
       headless: true,
       args: ["--no-sandbox"]
     });
-    const tab = await browser.newPage();
-    const text = await (await tab.goto("http://example.com/")).text();
-    console.log(text);
-    console.log("done");
-    browser.close();
-  }
+    const page = await browser.newPage();
+    await page.goto('http://cloud5.ren9ve.com.br/BIOCARE/Ren9veERP/CL_OrdensServicosNew.aspx');
+    await page.waitFor('input[name="ctl00$ContentPlaceHolder1$txtLogin"]');
+    await page.type('input[name="ctl00$ContentPlaceHolder1$txtLogin"]', 'oscarbio',{delay:100});
+    await page.type('input[name="ctl00$ContentPlaceHolder1$txtSenha"]', 'ger3729',{delay:100});
+    await page.keyboard.press('Enter');
+    await page.click('#ContentPlaceHolder1_btnLogin'); 
+
+    await filtrosDatas(dataInicial, dataFinal).then(() => filtrosOSFutura().then(() => numeroCliente()));
+    
+
+
+    async function filtrosDatas(dataInicial, dataFinal){
+        await page.waitForSelector('#ContentPlaceHolder1_AppContentPlaceHolder_lnkMaisOpcoes') //Link filtros
+        await page.click('#ContentPlaceHolder1_AppContentPlaceHolder_lnkMaisOpcoes',{delay:50});
+    
+        await page //0 - abertura // 2 - Finalizado
+        .waitForSelector('#ContentPlaceHolder1_AppContentPlaceHolder_rblData_2')
+        .then(() => page.click('#ContentPlaceHolder1_AppContentPlaceHolder_rblData_0',{delay:1000}));
+        //Datas
+        await page.type('input[name="ctl00$ctl00$ContentPlaceHolder1$AppContentPlaceHolder$txtDataInicial"]', dataInicial, {delay:100});
+        await page.type('input[name="ctl00$ctl00$ContentPlaceHolder1$AppContentPlaceHolder$txtDataFinal"]', dataFinal, {delay:100});
+    }
+      
+    async function filtrosOSFutura(){
+        await page.select('#ContentPlaceHolder1_AppContentPlaceHolder_drlCodOrdensServicosNewStatusTipos' ,'23');//tipo
+        await page.waitFor(3000)
+        .then(() => page.click('#ctl00_ctl00_ContentPlaceHolder1_AppContentPlaceHolder_drlCodOrdensServicosNewStatus_Arrow',{delay:200})) //dropdown
+        .then(() => page.waitFor(3000))
+        .then(() => page.waitForSelector('#ctl00_ctl00_ContentPlaceHolder1_AppContentPlaceHolder_drlCodOrdensServicosNewStatus_DropDown > div > div > label > input')) 
+        .then(() => page.click('#ctl00_ctl00_ContentPlaceHolder1_AppContentPlaceHolder_drlCodOrdensServicosNewStatus_DropDown > div > div > label > input',{delay:200})) //Check All
+        .then(() => page.click('#ContentPlaceHolder1_AppContentPlaceHolder_btnPesquisar', {delay:200}))
+        .then(() => page.click('#ContentPlaceHolder1_AppContentPlaceHolder_dgdOrdensServicosNew_lnkPosicaoCliente_0 > i')); //cliente toten
+    }
+
+    async function numeroCliente(){
+        await page.waitFor(4000);
+        const number = await page.evaluate(function() {
+            return document.querySelector("#ContentPlaceHolder1_AppContentPlaceHolder_modalposicaocliente_lblTelefone").textContent.replace(/[^\d]+/g,'');
+        });
+        var numberDDD = "55"+number;
+        console.log(numberDDD);
+        return numberDDD;
+    }
+}
   
 
 
 //Routes:
-app.use('/admin', admin)
 
