9fc93d0704529cd0842f1c3bcb9b609e1ab99d43
Oscar Gross Jr <oscar.gross222@gmail.com>
2020-10-04 18:44:46 -0300

    Creation project and install dependecies/index with route of scraping

---
+++ browser.js
@@ -0,0 +1,16 @@
+const puppeteer = require('puppeteer');
+
+module.exports = class Browser{
+
+
+
+    static async initPage(){
+        return puppeteer.launch({
+            headless: true,
+            args: ["--no-sandbox"]
+        });
+
+    };
+
+};
+
--- public/js/script.js
+++
@@ -1,8 +0,0 @@
-console.log("oiiii");
-        var button = document.querySelector("button");
-        button.addEventListener("click", function () {
-            var dataInicial = document.getElementById('dataInicial').textContent;
-            var dataFinal = document.getElementById('dataFinal').textContent;
-            console.log(dataInicial);
-
-        });
