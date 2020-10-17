60c64c436382656c911b62684e5d226edd3b85b8
Oscar Gross Jr <oscar.gross222@gmail.com>
2020-10-16 23:34:37 -0300

    add function whats

--- browser.js
+++ browser.js
@@ -3,12 +3,12 @@ const puppeteer = require('puppeteer');
 module.exports = class Browser{
 
 
-
     static async initPage(){
         return puppeteer.launch({
             headless: true,
             args: ["--no-sandbox"]
         });
+
 
     };
 
